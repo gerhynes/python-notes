c5f339f664172ae5694caeab2c1e280fbf4a3219
Gerard Hynes <gkhynes@gmail.com>
2021-03-05 22:05:37 +0000

    Add v2 of rock paper scissors

--- rock-paper-scissors/v1.py
+++ rock-paper-scissors/v1.py
@@ -3,6 +3,7 @@ print("Paper...")
 print("Scissors...")
 
 player1 = input("Player 1, make your move: ")
+print("***NO CHEATING!***\n\n" * 20)
 player2 = input("Player 2, make your move: ")
 
 if player1 == player2:
@@ -16,7 +17,7 @@ elif player1 == "paper":
     if player2 == "rock":
         print("Player1 wins!")
     elif player2 == "scissors":
-        print("Player 2 wins!")
+        print("Player2 wins!")
 elif player1 == "scissors":
     if player2 == "paper":
         print("Player1 wins!")
